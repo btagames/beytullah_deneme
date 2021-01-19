42c57b3c659bcc0089f3d51d2844871d73dc85f5
aydinbeytullah <61052929+aydinbeytullah@users.noreply.github.com>
2021-01-19 16:43:17 +0300

    Test App Added

---
+++ Assets/heceCumle/Scripts/heceCumleController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class heceCumleController : MonoBehaviour
+{
+
+
+    public GameObject itemParent, Item, bosItem;
+    public GameObject ItemKonum;
+    public List<GameObject> _bosObjeList = new List<GameObject>();
+    public List<soruClass> _soruList = new List<soruClass>();
+    [System.Serializable]
+    public class soruClass
+    {
+        public List<string> _kelimeler = new List<string>();
+        public List<string> _sesGecenKelime = new List<string>();
+        public int kelimeSayisi;
+        public AudioClip cumleSesi;
+        public string dogruCevap;
+
+    }
+    public List<string> _secilenKelimelerList = new List<string>();
+    public int rndSoru;
+
+    private int soruSayisi;
+    public int dogruSay;
+    public string kodID;
+
+    void Start()
+    {
+        kelimeAdd();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void kelimeAdd()
+    {
+        for (int i = 0; i < _soruList.Count; i++)
+        {
+            GameObject obj = Instantiate(Item, Item.transform.position, Quaternion.identity);
+            obj.transform.SetParent(itemParent.transform);
+            obj.transform.localScale = new Vector3(1, 1, 1);
+            int randomKelime = Random.Range(0, _soruList[i]._sesGecenKelime.Count);
+            int secilenKelime = int.Parse(_soruList[i]._sesGecenKelime[randomKelime]);
+            obj.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = _soruList[i]._kelimeler[secilenKelime];
+            obj.name = _soruList[i]._kelimeler[secilenKelime];
+            _secilenKelimelerList.Add(obj.name);
+        }
+
+        StartCoroutine(soruAdd());
+    }
+
+
+
+    IEnumerator soruAdd()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        rndSoru = Random.Range(0, _soruList.Count);
+
+        for (int i = 0; i < _soruList[rndSoru]._kelimeler.Count; i++)
+        {
+            string kelimeAra = _secilenKelimelerList.Where(x => x.Equals(_soruList[rndSoru]._kelimeler[i])).FirstOrDefault();
+            Debug.Log(kelimeAra);
+
+            if(kelimeAra == null)
+            {
+
+                GameObject obj = Instantiate(Item, Item.transform.position, Quaternion.identity);
+                obj.transform.SetParent(ItemKonum.transform);
+                obj.transform.localScale = new Vector3(1, 1, 1);
+                obj.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = _soruList[rndSoru]._kelimeler[i];
+                obj.name = _soruList[rndSoru]._kelimeler[i];
+
+
+
+
+            }
+            else
+            {
+                GameObject obj = Instantiate(bosItem, bosItem.transform.position, Quaternion.identity);
+                obj.transform.SetParent(ItemKonum.transform);
+                obj.transform.localScale = new Vector3(1, 1, 1);
+                obj.name = _soruList[rndSoru]._kelimeler[i];
+            }
+        }
+    }
+}
+  
+
+
---
+++ Assets/heceCumle/Scripts/heceCumleDrag.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class heceCumleDrag : EventTrigger
+{
+    public int neededindex;
+    public GameObject control;
+    public Vector3 ba;
+    public int ansright;
+    public Collider2D col;
+    public GameObject canvas,itemParent,konum;
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+        ba = transform.position;
+        this.transform.SetParent(canvas.transform);
+        itemParent.GetComponent<GridLayoutGroup>().enabled = false;
+    }
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        shouldLerp = false;
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10f);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (this.gameObject.layer != collision.gameObject.layer)
+        {
+            Physics2D.IgnoreCollision(this.gameObject.GetComponent<BoxCollider2D>(), collision);
+        }
+        if (collision.name == "konum")
+        {
+            col = collision;
+            ansright = 1;
+        }
+        else
+            ansright = -1;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (this.gameObject.layer != collision.gameObject.layer)
+        {
+            Physics2D.IgnoreCollision(this.gameObject.GetComponent<BoxCollider2D>(), collision);
+        }
+        if (collision.name == "konum")
+        {
+            col = collision;
+            ansright = 1;
+        }
+        else
+            ansright = -1;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        ansright = 0;
+    }
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+        if (ansright == 1)
+        {
+           // konum.GetComponent<kelimeKonumController>()._konumList.Add(ba);
+          //  konum.GetComponent<kelimeKonumController>()._indexList.Add(this.transform.GetSiblingIndex());
+           // col.gameObject.GetComponent<kelimeKonumController>().cevapVer();
+            this.gameObject.transform.position = col.transform.position;
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            this.GetComponent<EventTrigger>().enabled = false;
+            this.gameObject.transform.SetParent(col.transform);
+
+        }
+        else if (ansright == 0)
+        {
+          //  itemParent.GetComponent<GridLayoutGroup>().enabled = true;
+            this.transform.SetParent(itemParent.transform);
+            transform.position = ba;
+        }
+        else if (ansright == -1)
+        {
+           // itemParent.GetComponent<GridLayoutGroup>().enabled = true;
+            this.transform.SetParent(itemParent.transform);
+            transform.position = ba;
+        }
+    }
+    public void cevapYanlis()
+    {
+        //Debug.Log("CALİSTİ");
+  
+        this.transform.SetParent(itemParent.transform);
+        transform.position = ba;
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+    void Start()
+    {
+        konum = GameObject.Find("konum");
+        canvas = GameObject.Find("Canvas");
+        itemParent = GameObject.Find("ItemParent");
+        control = GameObject.Find("control");
+        Input.multiTouchEnabled = false;
+    }
+
+
+    private bool shouldLerp = false;
+    public float timeStartedLerping;
+    public float lerpTime = 0.5f;
+    public Vector2 bi;
+    public void startLerping()
+    {
+        timeStartedLerping = Time.time;
+        shouldLerp = true;
+
+        this.GetComponent<BoxCollider2D>().enabled = true;
+        this.GetComponent<EventTrigger>().enabled = true;
+
+    }
+    public Vector3 Lerp(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 0.5f)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+        var result = Vector3.Lerp(start, end, percentageComplete);
+        return result;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldLerp)
+        {
+            transform.position = Lerp(bi, ba, timeStartedLerping, lerpTime);
+        }
+    }
+}
